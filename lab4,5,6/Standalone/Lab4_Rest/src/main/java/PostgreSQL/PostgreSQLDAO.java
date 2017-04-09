35e2ecf646e7ad84403d3ec9a535a686d075c223
Tristar666 <dimaos92@mail.ru>
2017-04-09 23:21:32 +0300

    Lab 4,5,6

---
+++ lab4,5,6/Standalone/Lab4_Rest/src/main/java/PostgreSQL/PostgreSQLDAO.java
@@ -0,0 +1,184 @@
+package PostgreSQL;
+
+import Teams.*;
+import TeamResource.DataError;
+import connectionUtils.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+public class PostgreSQLDAO {
+    
+    private boolean check_field(String str){
+        switch(str){
+            case "city": return true;
+            case "id": return true;
+            case "name": return true;
+            case "stadium": return true;
+            case "cups": return true;
+            case "foundation": return true;
+            default: return false;
+        }
+    }
+    
+    public boolean checkSelect(String jsonString) throws JSONException{
+        JSONObject jsonObj = new JSONObject(jsonString);
+            Iterator<?> keys = jsonObj.keys();
+            while(keys.hasNext() ) {
+                String keyStr = (String)keys.next();
+                if (!check_field(keyStr.substring(0,keyStr.length()-1)))
+                    return false;
+                Object keyvalue = jsonObj.get(keyStr);
+                if (keyStr.equals("cups") || keyStr.equals("foundation")){
+                    if (!validateInt((String) keyvalue))
+                        return false;
+                }
+            }
+        return true;
+    }   
+    
+    public String parseSelect(String jsonString) throws JSONException, DataError{
+        String temp = "";
+        JSONObject jsonObj = new JSONObject(jsonString);
+        Iterator<?> keys = jsonObj.keys();
+        while(keys.hasNext() ) {
+            String keyStr = (String)keys.next();
+            if (!check_field(keyStr.substring(0,keyStr.length()-1)))
+                throw new DataError("Data error!!!");
+            Object keyvalue = jsonObj.get(keyStr);
+            if (keyStr.equals("cups") || keyStr.equals("foundation")){
+                    if (!validateInt((String) keyvalue))
+                        throw new DataError("Data error!!!");
+                }
+            temp += keyStr+"'"+keyvalue+"'"+"and ";
+        }
+        return temp.substring(0,temp.length()-4);
+    }
+    
+    public boolean validateInt(String data){
+        for (int i=0;i<data.length();++i){
+            if ((int)data.charAt(i) < 48 || (int)data.charAt(i) > 57){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public String parseInsert(String jsonString) throws DataError, JSONException{
+        String temp = "";
+        JSONObject jsonObj = new JSONObject(jsonString);
+        Iterator<?> keys = jsonObj.keys();
+        while(keys.hasNext() ) {
+            String keyStr = (String)keys.next();
+            if (!check_field(keyStr))
+                throw new DataError("Wrong field!!!");
+            Object keyvalue = jsonObj.get(keyStr);
+            if (keyStr.equals("cups") || keyStr.equals("foundation")){
+                if (!validateInt((String) keyvalue))
+                    throw new DataError("Wrong data type!!!");
+            }
+            temp += "'"+keyvalue+"'"+",";
+        }
+        return temp.substring(0,temp.length()-1);
+    }
+    
+    public String parseUpdate(String jsonString) throws JSONException, DataError {
+        String temp = "";
+        JSONObject jsonObj = new JSONObject(jsonString);
+        Iterator<?> keys = jsonObj.keys();
+        while(keys.hasNext() ) {
+            String keyStr = (String)keys.next();
+            if (!check_field(keyStr))
+                throw new DataError("Wrong field!!!");
+            Object keyvalue = jsonObj.get(keyStr);
+            if (keyStr.equals("cups") || keyStr.equals("foundation")){
+                if (!validateInt((String) keyvalue))
+                    throw new DataError("Wrong data type!!!");
+            }
+            if (keyStr.equals("id")){
+                if (!validateInt((String) keyvalue))
+                    throw new DataError("Wrong data type!!!");
+                temp = temp.substring(0,temp.length()-1);
+                temp += "where "+keyStr+"='"+keyvalue+"'";
+            } else temp += keyStr+"='"+keyvalue+"'"+",";
+        }
+        return temp;
+    }
+    
+    public String deleteTeam(String parametrs){
+        int res = -1;
+        Connection conn = null;
+        try {
+            conn = ConnectionUtil.getConnection();
+            PreparedStatement stmt = conn.prepareStatement("delete from teams where id="+parametrs);
+            res = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgreSQLDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return Integer.toString(res);
+    }
+    
+    public String updateTeam(String parametrs){
+        int res = -1;
+        Connection conn = null;
+        try {
+            conn = ConnectionUtil.getConnection();
+            PreparedStatement stmt = conn.prepareStatement("update teams set "+parametrs);
+            res = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgreSQLDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return Integer.toString(res);
+    }
+    
+    public String insertTeam(String parametrs){
+        int key = -1;
+        Connection conn = null;
+        try {
+            conn = ConnectionUtil.getConnection();
+            PreparedStatement stmt = conn.prepareStatement("insert into teams (name,city,stadium,cups,foundation) values("+parametrs+")",Statement.RETURN_GENERATED_KEYS);
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next())
+                key = rs.getInt(1);
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgreSQLDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return Integer.toString(key);
+    }
+    
+    public List<Team> getTeams(String parametrs) {
+        List<Team> teams = new ArrayList<>();
+        Connection connection = null;
+        try {
+            connection = ConnectionUtil.getConnection();
+            Statement stmt = connection.createStatement();
+            String sql = "select * from teams";
+            if (!parametrs.isEmpty()){
+                    sql += " where "+parametrs; 
+            }    
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String city = rs.getString("city");
+                String stadium = rs.getString("stadium");
+                int foundation = rs.getInt("foundation");
+                int cups = rs.getInt("cups");
+                Team team = new Team(id,name, city,stadium,foundation, cups);
+                teams.add(team);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgreSQLDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    return teams;
+    }   
+}
---
+++ lab4,5,6/Standalone/Lab4_Rest/src/main/java/TeamResource/TeamResource.java
@@ -0,0 +1,76 @@
+package TeamResource;
+
+import PostgreSQL.*;
+import Teams.Team;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import org.codehaus.jettison.json.*;
+
+@Path("/teams")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class TeamResource {
+
+    private boolean auth(String login, String pass){
+        return login.equals("lol") && pass.equals("lol");
+    }
+    
+    @GET
+    public List<Team> getTeams(@QueryParam("name") String jsonString,@HeaderParam("Login") String login, @HeaderParam("Pass") String pass) throws DataError, JSONException {
+        if (!auth(login,pass))
+            throw new DataError("Wrong password or login!!!");
+        jsonString = jsonString.replaceAll("\\+", " "); //encoding problem
+        PostgreSQLDAO dao = new PostgreSQLDAO();
+        jsonString = dao.parseSelect(jsonString);
+        List<Team> persons = dao.getTeams(jsonString);
+        return persons;
+    }
+    
+    @PUT
+    public String insertTeam(@QueryParam("name") String jsonString, @HeaderParam("Login") String login, @HeaderParam("Pass") String pass) throws DataError, JSONException{
+        if (!auth(login,pass))
+            throw new DataError("Wrong password or login!!!");
+        jsonString = jsonString.replaceAll("\\+", " "); //encoding problem
+        PostgreSQLDAO dao = new PostgreSQLDAO();
+        jsonString = dao.parseInsert(jsonString);
+        String res = dao.insertTeam(jsonString);
+        if (Integer.parseInt(res) < 0)
+            throw new DataError("Insert problem...");
+        return res;
+    }
+    
+    @DELETE
+    public String deleteTeam(@QueryParam("id") String id,@HeaderParam("Login") String login, @HeaderParam("Pass") String pass) throws DataError{
+        if (!auth(login,pass))
+            throw new DataError("Wrong password or login!!!");
+        PostgreSQLDAO dao = new PostgreSQLDAO();
+        if (!dao.validateInt(id))
+            throw new DataError("Wrong data type!!!");
+        String res = dao.deleteTeam(id);
+        if (Integer.parseInt(res) < 0)
+            throw new DataError("Delete problem...");
+        if (Integer.parseInt(res) == 0)
+                throw new DataError("No data with this id...");
+        return res;
+    }
+    
+    @POST
+    public String updateTeam(@QueryParam("name") String jsonString, @HeaderParam("Login") String login, @HeaderParam("Pass") String pass) throws DataError, JSONException{
+        if (!auth(login,pass))
+            throw new DataError("Wrong password or login!!!");
+        jsonString = jsonString.replaceAll("\\+", " "); //encoding problem
+        PostgreSQLDAO dao = new PostgreSQLDAO();
+        jsonString = dao.parseUpdate(jsonString);
+        String res = dao.updateTeam(jsonString);
+        if (Integer.parseInt(res) <= 0)
+            throw new DataError("Update problem...");
+        return res;
+    }
+}
